0efe3b7f6a06a027539e91209306f19b30062bca
Deepak290701 <50839672+Deepak290701@users.noreply.github.com>
2019-11-20 21:58:14 +0530

    Add files via upload

--- student management system.c
+++ student management system.c
@@ -1,79 +1,319 @@
 #include<stdio.h>
-#include<conio.h>
-#include<string.h>
 
-void add(){
-    char name[100],address[200];
-    int rollno;
-    int choic=1;
-    int phoneno;
-    system("cls");
-    FILE *fp;
-    fp=fopen("studen.txt","a");
-    while(choic > 0  || choic < 0 ){
-        system("cls");
-        printf("Enter  Name    Roll No.   Phone No.      Address  \n");
-        scanf(" %s   %d    %d   %s",&name,&rollno,&phoneno,&address);
-        printf("\n Successfully Added\n");
-        printf("\n To add another entry enter a number other than 0 :- \n");
-        scanf("%d",&choic);
-    }
-    fclose(fp);
+struct student
+{
+ int rollno;
+ char name[30];
+ float mark;
+}stud;
+
+//    FUNCTION TO INSERT RECORDS TO THE FILE
+
+void insert()
+{
+ system("cls");
+ unsigned int avl,ptanhi;
+ FILE *fp;
+ fp = fopen("Record", "a");
+ printf("Enter the Roll no in range 1 to 10000000   :");
+ scanf("%d", &ptanhi/*&stud.rollno*/);
+ avl=avlrollno(ptanhi);
+ while(avl==1){
+    printf("Enter the UNIQUE Roll no   : ");
+    scanf("%d", &ptanhi);
+    avl=avlrollno(ptanhi);
+ }
+ stud.rollno=ptanhi;
+ printf("Enter the Name      :");
+ scanf("%s", &stud.name);
+ printf("Enter the mark      :");
+ scanf("%f", &stud.mark);
+
+ fwrite(&stud, sizeof(stud), 1, fp);
+ fclose(fp);
+ getch();
 }
 
+//    FUNCTION TO DISPLAY RECORDS
 
-void print(){
-    system("cls");
-    FILE *fp;
-    char name[100][100],address[100][200];
-    int rollno[100];
-    int phoneno[100];
-    int i=0;
-    fp=fopen("studen.txt","r");
-    printf(" Name    Roll No.   Phone No.        Address  \n");
-    while(!feof(fp)){
-        fscanf(fp,"  %s  %d  %d  %s \n",&name[i],&rollno[i],&phoneno[i],&address[i]);
-        printf("%s \t\t %d \t\t %d \t\t %s \n",name[i],rollno[i],phoneno[i],address[i]);
-        i++;
-    }
-    fclose(fp);
-    getch();
+void disp()
+{
+ system("cls");
+ FILE *fp1;
+ fp1 = fopen("Record", "r");
+ printf("\nRoll Number\tName\tMark\n\n");
+ while (fread(&stud, sizeof(stud), 1, fp1))
+    printf("  %d\t\t%s\t%.2f\n", stud.rollno, stud.name, stud.mark);
+ fclose(fp1);
+ getch();
 }
 
+//    FUNCTION TO SEARCH THE GIVEN RECORD
 
-void main(){
+void search()
+{
+ system("cls");
+ FILE *fp2;
+ int r, s, avl;
+ printf("\nEnter the Roll no you want to search  :");
+ scanf("%d", &r);
+ avl = avlrollno(r);
+ if (avl == 0)
+  printf("Roll No %d is not available in the file\n",r);
+ else
+ {
+  fp2 = fopen("Record", "r");
+  while (fread(&stud, sizeof(stud), 1, fp2))
+  {
+   s = stud.rollno;
+   if (s == r)
+   {
+    printf("\n\tRoll no = %d", stud.rollno);
+    printf("\n\tName    = %s", stud.name);
+    printf("\n\tMark    = %.2f\n", stud.mark);
+   }
+  }
+  fclose(fp2);
 
-    int choice=1;
-    while(choice){
-        system("cls");
+ }
+ getch();
+}
+
+//    FUNCTION TO DELETE A RECORD
+
+
+void deletefile()
+{
+ system("cls");
+ FILE *fpo;
+ FILE *fpt;
+ int r, s;
+ printf("Enter the Roll no you want to delete :");
+ scanf("%d", &r);
+ if (avlrollno(r) == 0)
+  printf("Roll no %d is not available in the file\n", r);
+ else
+ {
+  fpo = fopen("Record", "r");
+  fpt = fopen("TempFile", "w");
+  while (fread(&stud, sizeof(stud), 1, fpo))
+  {
+   s = stud.rollno;
+   if (s != r)
+    fwrite(&stud, sizeof(stud), 1, fpt);
+  }
+  fclose(fpo);
+  fclose(fpt);
+  fpo = fopen("Record", "w");
+  fpt = fopen("TempFile", "r");
+  while (fread(&stud, sizeof(stud), 1, fpt))
+   fwrite(&stud, sizeof(stud), 1, fpo);
+  printf("\nRECORD DELETED\n");
+  fclose(fpo);
+  fclose(fpt);
+ }
+getch();
+}
+
+//    FUNCTION TO UPDATE THE RECORD
+
+void update()
+{
+ system("cls");
+ int avl;
+ FILE *fpt;
+ FILE *fpo;
+ int s, r, ch;
+ printf("Enter roll number to update:");
+ scanf("%d", &r);
+ avl = avlrollno(r);
+ if (avl == 0)
+ {
+  printf("Roll number %d is not Available in the file", r);
+ }
+ else
+ {
+  fpo = fopen("Record", "r");
+  fpt = fopen("TempFile", "w");
+  while (fread(&stud, sizeof(stud), 1, fpo))
+  {
+   s = stud.rollno;
+   if (s != r)
+    fwrite(&stud, sizeof(stud), 1, fpt);
+   else
+   {
+    printf("\n\t1. Update Name of Roll Number %d", r);
+    printf("\n\t2. Update Mark of Roll Number %d", r);
+    printf("\n\t3. Update both Name and Mark of Roll Number %d", r);
+    printf("\nEnter your choice:");
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+     printf("Enter Name:");
+     scanf("%s", &stud.name);
+     break;
+    case 2:
+     printf("Enter Mark : ");
+     scanf("%f", &stud.mark);
+     break;
+    case 3:
+     printf("Enter Name: ");
+     scanf("%s", &stud.name);
+     printf("Enter Mark: ");
+     scanf("%f", &stud.mark);
+     break;
+    default:
+     printf("Invalid Selection");
+     break;
+    }
+    fwrite(&stud, sizeof(stud), 1, fpt);
+   }
+  }
+  fclose(fpo);
+  fclose(fpt);
+  fpo = fopen("Record", "w");
+  fpt = fopen("TempFile", "r");
+  while (fread(&stud, sizeof(stud), 1, fpt))
+  {
+   fwrite(&stud, sizeof(stud), 1, fpo);
+  }
+  fclose(fpo);
+  fclose(fpt);
+  printf("RECORD UPDATED");
+ }
+ getch();
+}
+
+/* FUNCTION TO SORT THE RECORD */
+
+void sort()
+{
+ system("cls");
+ int a[20], count = 0, i, j, t, c;
+ FILE *fpo;
+ fpo = fopen("Record", "r");
+ while (fread(&stud, sizeof(stud), 1, fpo))
+ {
+  a[count] = stud.rollno;
+  count++;
+ }
+ c = count;
+ for (i = 0; i<count - 1; i++)
+ {
+  for (j = i + 1; j<count; j++)
+  {
+   if (a[i]>a[j])
+   {
+    t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+   }
+  }
+ }
+ printf("Roll No.\tName\t\tMark\n");
+ count = c;
+ for (i = 0; i<count; i++)
+ {
+  rewind(fpo);
+  while (fread(&stud, sizeof(stud), 1, fpo))
+  {
+   if (a[i] == stud.rollno)
+    printf("\n %d\t\t %s \t\t %2f",stud.rollno, stud.name, stud.mark);
+  }
+
+ }
+ getch();
+}
+//    FUNCTION TO CHECK GIVEN ROLL NO IS AVAILABLE //
+int avlrollno(int rno)
+{
+ FILE *fp;
+ int c = 0;
+ fp = fopen("Record", "r");
+ while (!feof(fp))
+ {
+  fread(&stud, sizeof(stud), 1, fp);
+
+  if (rno == stud.rollno)
+  {
+   fclose(fp);
+   return 1;
+  }
+ }
+ fclose(fp);
+ return 0;
+}
+
+//FUNCTION TO CHECK THE FILE IS EMPTY OR NOT
+
+int empty()
+{
+    int c = 0;
+    FILE *fp;
+    fp = fopen("Record", "r");
+    while (fread(&stud, sizeof(stud), 1, fp))
+    c = 1;
+    fclose(fp);
+    return c;
+}
+
+void main()
+{
+ int c, emp;
+ do
+ {
+    system("cls");
     printf("                                             STUDENT MANAGEMENT SYSTEM        \n");
-    printf("________________________________________________________________________________________________________________________\n\n");
-    printf("Main Menu:--\n\n");
-    printf("  1.)Add New Student Details \n");
-    printf("  2.)Print List Of All Existing Students \n");
-    printf("  3.)Print Details of the Student Using Roll No.\n");
-    printf("  4.)Add New Student Details \n");
-    printf("  5.)Add New Student Details \n");
-    printf("  0.)EXIT\n");
-    printf(" \nEnter Your Choice:--\n");
-    scanf("%d",&choice);
-        switch(choice){
-            case 0:return ;
-                    break;
-            case 1: add();
-                    break;
-            case 2: print();
-                    break;
-            case 3:
-                    break;
-            default:
-                    break;
-
-        }
-        printf("\n press 0 for exit else 1  :- ");
-        scanf("%d",&choice);
-        if(choice==0)
+    printf("________________________________________________________________________________________________________________________");
+    printf("\n---Select your choice---------\n");
+    printf("\n\t1. INSERT\n\t2. DISPLAY\n\t3. SEARCH");
+    printf("\n\t4. DELETE\n\t5. UPDATE\n\t6. SORT");
+    printf("\n\t7. EXIT");
+    printf("\n\n------------------------------------------\n");
+    printf("\nEnter your choice:");
+    scanf("%d", &c);
+    printf("\n");
+    switch (c)
+    {
+        case 1:
+            insert();
+            break;
+        case 2:
+            emp = empty();
+            if (emp == 0){
+                printf("\nThe file is EMPTY\n");
+                getch();
+            }
+            else
+                disp();
+            break;
+        case 3:
+            search();
+            break;
+        case 4:
+            deletefile();
+            break;
+        case 5:
+            update();
+            break;
+        case 6:
+            emp = empty();
+            if (emp == 0){
+                printf("\n The file is EMPTY\n");
+                getch();
+            }
+            else
+                sort();
+            break;
+        case 7:
+            exit(1);
+            break;
+        default:
+            printf("\nYour choice is wrong\nPlease try again...\n");
+            getch();
             break;
 
-    }
+        }
+    } while (c != 7);
 }
